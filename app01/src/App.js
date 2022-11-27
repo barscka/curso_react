57cc23390236f3901e3787cc198bea008d1278f4
Alisson Oliveira <alissonof@uol.com.br>
2022-11-27 12:13:34 -0300

    state

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import Header from './components/Header';
 import Corpo from './components/Corpo';
+import Numero from './components/Numero';
 
 
 export default function App() {
+  const [num,setNum]=useState(10)
+  
 
   return(
-    <>
+    <>  
     <Header></Header>
     <Corpo/>
-    
+    <p> Valor do state num em App: {num}</p>
+    <Numero num={num} setNum={setNum}/>
     </>
   )
 }
